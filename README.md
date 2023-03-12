b95a06e5b372dfb70af17605d9aad7a1c53b7b66
nohatekate <kateiscoding@gmail.com>
2023-03-11 19:42:18 -0600

    update ReadMe and cleaned up JS code

--- README.md
+++ README.md
@@ -15,8 +15,12 @@ game is over when all cards match or if player can't match in time
 use timer for tracking how long it takes player to make all matching pairs
 keep track of best time
   - flip animation to show images
-  - 20 card (option between 8 / 12 / 20)
-  - CSS allow for small
+  - flip animation to automatically flip card to not showing when guess is incorrect
+  - 20 card (option for number of cards to match)
+  - CSS allow for screen size changes / media queries
+
+**Known Bugs**
+  - Can reclick on an already matched pair repeatedly and win game on same matching pair
 
 https://excalidraw.com/#json=H9_7MkksRv6yMVjNMy1DM,t-7nJhcbdDbQpFclek9vzA
 
--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 /*----- constants -----*/
 //cards for game play
-//array of random ordered cards
+//array of "random" ordered cards
 const cards = ["🖤", "🙄", "💯", "🌈", "💯", "🌈", "🖤", "🙄", "🙈", "🫠", "🍑", "🐽", "😍", "🔥", "🐽", "😍", "🔥", "🍑", "🫠", "🙈"];
 const maxGuesses = 5;
 
 /*----- state variables -----*/
+//game table to place cards face down
 let gameTable = [];
 // when array has 2 cards === full -> compare
 let cardsMatch = [];
 let pairsMatched;
+//every 2 clicks needs to add up to "1 move" and "minus 1 guess if no match"
 let clickCount;
-//every 2 clicks needs to add up to "1 move" and "minus 1 guess"
 let guessesLeft;
 
 /*----- cached elements  -----*/
 const playAgainEl = document.querySelector("button")
-const countEl = document.querySelector(".number-of-moves")
 const messageEl = document.querySelector(".message-updates")
 const cardEls = document.querySelectorAll(".card")
 const pairsMatchedCountEl = document.querySelector(".number-of-pairs")
@@ -25,12 +25,12 @@ const guessesLeftEl = document.querySelector(".guesses-left")
 // need to be able to click on cards - show image - compare two cards - decide if they will stay or images hide again
 //click on each card
 for (let i = 0; i < cardEls.length; i++) {
+    //card elements to loop over to add event listener
     const currentCardEl = cardEls[i]
     currentCardEl.addEventListener("click", handleCardClick)
 }
 
 // need to be able to click the play again button to reset game
-//array of card elements to loop over to add event listener
 playAgainEl.addEventListener("click", init)
 
 /*----- functions -----*/
@@ -46,18 +46,16 @@ function init() {
     pairsMatchedCountEl.innerHTML = pairsMatched;
     guessesLeftEl.innerHTML = maxGuesses;
     messageEl.innerText = "🧠 Test your Brain Space! 🧠"
-
     render();
 }
 
 function render() {
-    renderMessages();
     renderGameTable();
 }
 
 function renderGameTable() {
-    // // needs to track where cards are
-    //to set state (new game)
+    // needs to track where cards are
+    //to set state (new game and changes)
     //loop through the game table - if 0(meaning no card value) -> set card
     for (let i = 0; i < gameTable.length; i++) {
         const currentTableIdx = gameTable[i]
@@ -66,85 +64,36 @@ function renderGameTable() {
         //when 0 - set current index to the value of card[i]
         if (currentTableIdx === 1) {
             currentCardElIdx.innerHTML = currentEmoji;
-            // currentTableIdx.disabled = true;
         } else {
             currentCardElIdx.innerHTML = ""
         }
     }
-
 }
 
-
-
-function renderMessages() {
-    // if (clickCount++) {
-    //     messageEl.innerHTML = "You Clicked A Card!"
-    // }
-
-    //message for match made
-    // if two cards don't match messageEl.textContent === "No Match! Try Again!"
-    //message for no match
-    // if guessCount > maxGuesses messageEl.textContent === "Out of guesses! Play Again!"
-    //message for loss (over max guess count)
-    // if all cards match (no cards on table) messageEl.textContent === "Great job matching!"
-    //message for win (all matches made)
-
-}
-// do messages and checkMatch do the same thing??
-
-function checkMatch() {
-
-
-    //allow for two clicks on separate cards and then check for match
-    // if match made - make cards color of background to "disapear"
-    //if statement to compare two cards
-    // if match made message match made
-    // if no match message try again
-    // if all cards match message great job
-    // if guess count > max guess message you lose
-}
-
-function checkWinner() {
-
-    //Check the pairsMatched variable
-
-    //as cards match - do all cards match? all cards match < maxGuesses === winner
-}
-
-function gameOver() {
-    // if guessCount > maxGuesses == you lose, game over
-    // if all cards match == you win, game over, play again
-}
 function checkGuess() {
     if (clickCount === 2) {
         if (cardsMatch[0] === cardsMatch[1]) {
             // if two cards match 
             pairsMatched++;
             messageEl.innerText = "Great Match! Keep Going!";
-            // if (guessesLeft > 0) {
-            //     guessesLeft--;
-            // }
             handleGuess();
-
             if (pairsMatched === cards.length / 2) {
                 messageEl.innerHTML = "YOU WIN! 🏆"
             } else if (guessesLeft === 0) {
                 messageEl.innerText = "😝 YOU LOSE! START OVER! 😝"
             }
         } else {
+            //if two cards don't match
             messageEl.innerText = "No Matches! 👻 Guess Again!"
-            if (guessesLeft > 0) {
-                guessesLeft--;
-                // if guesses left is 0 then set message to you lose
-            }
+            guessesLeft--;
             if (guessesLeft === 0) {
                 messageEl.innerText = "😝 You're out of guesses! START OVER! 😝"
             }
-            //if guesses left is 0 
             handleGuess();
         }
     }
 }
+
 function handleGuess() {
     pairsMatchedCountEl.innerText = pairsMatched;
     guessesLeftEl.innerText = guessesLeft;
@@ -152,24 +101,22 @@ function handleGuess() {
     clickCount = 0;
 }
 
-// trying to figure out what happens when I click on my cards
-//how to only allow for two clicks and each click on separate cards
+//what happens when I click on my cards
+//only click on 2 cards - 2 cards must be different cards
 function handleCardClick(evt) {
     if (pairsMatched !== cards.length / 2 && guessesLeft !== 0) {
         if (gameTable[evt.target.id] === 0) {
-            //flipped card
+            //if card is not showing and we click - it shows
             gameTable[evt.target.id] = 1;
             clickCount++;
             cardsMatch.push(cards[evt.target.id])
             checkGuess();
-
         } else {
+            //if card is showing and we click - it stops showing
             gameTable[evt.target.id] = 0;
             cardsMatch = [];
             clickCount = 0;
         }
-        // disable click on that same card ???
-
     }
     render();
 }
